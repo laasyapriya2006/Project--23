a98b067fc2f60ce05d16535f7023e18dab68c587
laasyapriya2006 <68801698+laasyapriya2006@users.noreply.github.com>
2020-08-10 19:42:48 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,19 @@
+class Box{
+    constructor(x,y,width,height){
+        var box_options = {
+            isStatic: true
+        };
+        this.body = Bodies.rectangle(x,y,width,height,box_options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
